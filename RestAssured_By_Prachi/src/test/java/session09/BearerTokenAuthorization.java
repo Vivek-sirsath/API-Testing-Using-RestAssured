19339b10bd503a44b7e496de39ae8ac16c93aab7
vivek <vivek.sirsath1@gmail.com>
2023-08-01 22:44:15 +0530

    Additional info about bearer token is added.

--- RestAssured_By_Prachi/src/test/java/session09/BearerTokenAuthorization.java
+++ RestAssured_By_Prachi/src/test/java/session09/BearerTokenAuthorization.java
@@ -9,44 +9,43 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class BearerTokenAuthorization {
-	
+
 	@Test
 	public void bearerToken() {
 		// https://gorest.co.in/public/v2/users
-		
+
 		// create request specification
 		RequestSpecification requestSpec = RestAssured.given();
-		
+
 		// Specify baseUri and basePath
 		requestSpec.baseUri("https://gorest.co.in");
 		requestSpec.basePath("/public/v2/users");
-		
+
 		/*
-		  
-		  {
-			"name": "CodeKiller",
-		    "email": "Codekiller1@gmail.com",
-		    "gender": "male",
-		    "status": "active"
-		  }
-		
-		*/
-		
-		// We create JSON object to store the JSON data which we want to create on server
+		 * 
+		 * { "name": "CodeKiller", "email": "Codekiller1@gmail.com", "gender": "male",
+		 * "status": "active" }
+		 * 
+		 */
+
+		// We create JSON object to store the JSON data which we want to create on
+		// server
 		JSONObject payload = new JSONObject();
 		payload.put("name", "john");
 		payload.put("email", "john@gmail.com");
 		payload.put("gender", "male");
 		payload.put("status", "active");
-		
-		
-		// We will need token
+
+		// We will need a bearer token for authorization 
+		// And we need to store it in string
 		String AuthToken = "Bearer 064179c9fda7ce18a6038a50dce09e948e668e5c766571a8b899bf3ba89a614b";
-		
-		requestSpec.header("Authorization", AuthToken)
-		.contentType(ContentType.JSON)
-		.body(payload.toJSONString());
-		
+
+		// This token will go as a part of header, hence we need to create header 
+		// which will be in key-value pair.
+	    
+
+		requestSpec.header("Authorization", AuthToken).contentType(ContentType.JSON).body(payload.toJSONString());
+
 		// Perform POST request
 		Response response = requestSpec.post();
 		System.out.println("Response Line:- " + response.getStatusLine());
