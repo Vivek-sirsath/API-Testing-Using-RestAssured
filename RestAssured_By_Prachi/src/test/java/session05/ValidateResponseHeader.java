4cc64c0be3b11b38367954378bc84ade06ee3192
vivek <vivek.sirsath1@gmail.com>
2025-07-14 00:29:05 +0530

    API Key added in ValidateResponseHeader.java

--- RestAssured_By_Prachi/src/test/java/session05/ValidateResponseHeader.java
+++ RestAssured_By_Prachi/src/test/java/session05/ValidateResponseHeader.java
@@ -20,16 +20,23 @@ public class ValidateResponseHeader {
 		// Specify base URI
 		reqSpec.baseUri("https://reqres.in");
 		reqSpec.basePath("/api/users/2");
+		
+		// Specify API-KEY in header
+		reqSpec.header("x-api-key", "reqres-free-v1");
 
-		// Form Get request
+		// Perform Get request
 		Response resp = reqSpec.get();
 
 		// Validate response Header
+		
+		String xapikey = resp.getHeader("xapikey");
+		System.out.println("Value of xapikey: " + xapikey);  // null # This header is not returning with other response headers.
+		
 		String contentType = resp.getHeader("Content-Type");
-		System.out.println("Value of content-type: " + contentType);
+		System.out.println("Value of content-type: " + contentType);   // application/json; charset=utf-8
 
 		String connection = resp.getHeader("connection");
-		System.out.println("Value of connection: " + connection);
+		System.out.println("Value of connection: " + connection);   // keep-alive
 		System.out.println(" ");
 
 		// Read all headers and print their respective values
