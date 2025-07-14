05894b732763d65fa659f37c1c49de60be42686b
vivek <vivek.sirsath1@gmail.com>
2025-07-15 00:26:45 +0530

    Alignment of a comment is done in DigestAuth.java

--- RestAssured_By_Prachi/src/test/java/session08/DigestAuth.java
+++ RestAssured_By_Prachi/src/test/java/session08/DigestAuth.java
@@ -23,13 +23,16 @@ public class DigestAuth {
 		requestSpec.basePath("/digest-auth/undefined/vivek/1234");
 
 		// Create Get request with digest authentication credentials
-		Response response = requestSpec.auth().digest("vivek", "1234").get(); // If anything wrong then Status Line:- // HTTP/1.1 401 Unauthorized
+		Response response = requestSpec.auth().digest("vivek", "1234").get(); // If user-name or password wrong then Status Line:- // HTTP/1.1 401 UNAUTHORIZED
 
 		// print status line
 		System.out.println(" Digest Auth Status Line:- " + response.getStatusLine()); // HTTP/1.1 200 OK
 		System.out.println("Digest Auth Response Body:- " + response.body().asString());
 		/*
-		 * { "authenticated": true, "user": "vivek" }
+		    {
+              "authenticated": true,
+              "user": "vivek"
+            }
 		 */
 
 		// Validate response code
